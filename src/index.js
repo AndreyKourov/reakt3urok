25892ff41c305fcfc855bbe000a64c2f87cbfc56
AndreyKourov <andreykourov@mail.ru>
2020-04-16 19:38:02 +0600

    что то

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 
 class App extends Component {
     constructor() {
+        super();
+        this.state = {
+            page: "Что то с чем то"
+        };
+    }
+
+    render() {
         
+    
+    return (
+        <>
+            {this.state.page}
+        </>
+    );
     }
 }
+
+ReactDOM.render(<App/>,document.getElementById("root"))
